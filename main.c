a29dad5ab40c346a713f4773781302038bc36b1c
Danielgimp <57535036+Danielgimp@users.noreply.github.com>
2019-11-26 10:34:47 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include "myBank.h"
 
-    double main() {
+    int main() {
         char func;
         int account_number;
         double interest_rate=0;
         double amount=0;
+        
 
-        while (func!=-1) //69==E
+        while (func!=-1) //No Such Ascii Value --> means infinite loop
         {
 
         printf("Transaction type?:  \n");
@@ -18,14 +19,15 @@
                 {
                 case 79: 
                 printf("Initial deposit?: \n");
-                scanf(" %lf\n",&amount);
+                scanf(" %lf",&amount);
                 printf("The New Account No is: %d \n",O(amount));
                 break;
 
                 case 66:
                 printf("Account Number?: \n");
-                scanf(" %d\n",&account_number);
-                printf("The Balance On The Account is: %lf \n",B(account_number));
+                scanf(" %d",&account_number);
+                if(B(account_number)>0) printf("The Balance On The Account is: %0.2lf \n",B(account_number));
+                else printf("The account %d is closed and/or was never opened \n",account_number);
                 break;
 
                 case 68:
@@ -33,7 +35,7 @@
                 scanf(" %d",&account_number);
                 printf("Amount?: \n");
                 scanf(" %lf",&amount);
-                printf("The New Amount is: %lf",D(account_number,amount)); 
+                printf("The New Amount is: %0.2lf \n",D(account_number,amount)); 
                 break;
 
                 case 87:
@@ -41,17 +43,19 @@
                 scanf(" %d",&account_number);
                 printf("Amount?: \n");
                 scanf(" %lf",&amount);
-                printf("The New Amount is: %lf",W(account_number,amount));
+                if(W(account_number,amount)>=0) printf("The New Amount is: %0.2lf \n",W(account_number,amount));
+                else printf("\n");
                 break;
 
                 case 67:
                 printf("Account number?: \n");
-                scanf(" %d\n",&account_number);
+                scanf(" %d",&account_number);
                 C(account_number);
                 break;
 
                 case 73:
-                scanf("Interest rate?: %lf \n",&interest_rate);
+                printf("Input Interst Rate: \n");
+                scanf(" %lf",&interest_rate);
                 I(interest_rate);
                 break;
 
@@ -61,16 +65,17 @@
                 break;
 
                 case 69:
-               
-                E();
-                return 0;
-                break;
-               
+                 E();
+                 printf("All Accounts are closed, Program is finished running.");
+                 return 0;
+                                                              
                 default:
                 printf("Error! Input is incorrect \n");
                 break;
                 }
                 
         }
+        return 0;
+        
     }
     
--- myBank.c
+++ myBank.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 int AccountsNum=0;
 int accountId=900;
-
+//int accountId=900;
 
 
 int O (double amount) //open account and save initial deposit(iff acc# is smaller <=50) .print new account number.-works --no bug checks 
 {
-if(AccountsNum<50)
+if(AccountsNum<50 && amount>0)
 {
 bankMatrix[0][AccountsNum]=amount; //setup account with the given amount
 bankMatrix[1][AccountsNum]=accountId+1; //give an account id
@@ -15,10 +15,11 @@ accountId++; //uplift the account id
 AccountsNum++; //uplift the total account num
 return accountId;
 }
-else printf("There are alreay 50 accounts! \n");
+else return printf("There are alreay 50 accounts! OR there is a wrong input \n");
+
 }
 
-double B (int account_number) // take account number and return the balance (iff the acount is open) - works --no bug checks
+double B (int account_number) // take account number and return the balance (iff the acount is open) - works 
 {
 double balance;
 if((account_number-901)>AccountsNum || account_number-901<0 ) //if the given account is bigger than number of accounts theres nothing to do
@@ -30,6 +31,7 @@ else
 {
      //create balance var
     balance=bankMatrix[0][account_number-901]; //and just return what amount it has
+    
 
 }
 return balance;
@@ -37,9 +39,10 @@ return balance;
 
 double D (int account_number, double amount) // money deposit (iff the account is open), than print the new balance. -works --no bug checks
 {
-if((account_number-901)>AccountsNum || account_number-901<0 ) //if the given account is bigger than number of accounts theres nothing to do (or smaller than 901)
+if((account_number-901)<=AccountsNum || account_number-901<=0 || account_number-901>49) //if the given account is bigger than number of accounts theres nothing to do (or smaller than 901)
 {
     printf("There is no such account \n");
+    
 
 }
 else
@@ -48,19 +51,21 @@ else
     {
         if(bankMatrix[1][i]==account_number) //if you found the account (and it should!)
         {
+            
             bankMatrix[0][i]+=amount; //top up the account with the given amount 
             return bankMatrix[0][i]; //returns the new amount
         }
 
     }
 }
+return printf(" ");
 }
 
 double W (int account_number, double amount) //withdraw from account (iff the account is open && account balance is>=num) -works --no bug checks 
 {
-if((account_number-901)>AccountsNum || account_number-901<0 ) //if the given account is bigger than number of accounts theres nothing to do (or smaller than 901)
+if((account_number-901)<=AccountsNum || account_number-901<=0 || account_number-901>49 ) //if the given account is bigger than number of accounts theres nothing to do (or smaller than 901)
 {
-    printf("There is no such account \n");
+    printf("There is no such account or there are no funds on the account,Try again! \n");
 
 }
 else
@@ -84,6 +89,7 @@ else
 
     }
 }
+return printf(" ");
 }
 
 void C (int account_number) //close account (iff the account is open) -works
@@ -98,40 +104,50 @@ void C (int account_number) //close account (iff the account is open) -works
         bankMatrix[0][account_number-901]=0; //just go to the account# and set it to 0
         printf("The Account: %d Is Closed \n",account_number);
     }
+     accountId--;
+     AccountsNum--;
        
 }
 
-double I (double interest_rate) // intrest rate to all open accounts
+double I (double interest_rate) // intrest rate to all open accounts -works
 {
 double balance; //create balance var
-        for (int i=0;i<sizeof(bankMatrix[1]);i++) //iterate through all accounts
+        if(interest_rate>0)
+        {
+        for (int i=0;i<AccountsNum;i++) //iterate through all accounts
         {
         balance=bankMatrix[0][i]; //get each account balance
-        bankMatrix[0][i]=balance*(interest_rate/100);
-        if(balance>0)printf("The Current Amount in account %d is: %f \n",i+901,balance); //and print it
+        bankMatrix[0][i]=balance*(interest_rate/100)+balance;
+        if(balance>0)printf("The Current Amount in account %d is: %0.2lf \n",i+901,bankMatrix[0][i]); //and print it
         }
-    
+        }
+        else printf("The Interest rate has to be a positive number!");
+    return printf(" ");
 }
 
-double P () //print all open accounts and their balance -works --with bugs! 
+double P () //print all open accounts and their balance -works 
 {
     double balance; //create balance var
-        for (int i=0;i<sizeof(bankMatrix[1]);i++) //iterate through all accounts
+        for (int i=0;i<AccountsNum;i++) //iterate through all accounts
         {
         balance=bankMatrix[0][i]; //get each account balance
         if(balance>0)printf("The Current Ammount in account %d is: %f \n",i+901,balance); //and print it
         }
+        return printf(" ");
     
 }
 
-void E () //close all account and exit program -works --no bug checks
+void E () //close all account and exit program -works 
 {
     
         for (int i=0;i<sizeof(bankMatrix[1]);i++) //iterate through all accounts
         {
         bankMatrix[0][i]=0; //get each account balance
         bankMatrix[1][i]=0;
+        accountId--; 
+        AccountsNum--;
         }
+
     
     
 }
